eabc3457535aa2269c375f2fb3b3bdb4f2107a92
not-developer-exe <official.additya.singh@gmail.com>
2025-06-06 23:31:46 +0530

    Basic Topics Covered

---
+++ Day-1/app.js
@@ -0,0 +1,10 @@
+// Even adding two numbers matters — because it’s the small steps, done consistently, that build the strongest foundations.
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+// Difference between number and string.
+
+let c = 10; // Number
+let d = '10'; // String
