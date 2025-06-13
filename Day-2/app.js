050ee7dc00a880c35d471c8e31bc2e7372807e20
not-developer-exe <official.additya.singh@gmail.com>
2025-06-13 12:26:24 +0530

    Math Function started

--- Day-2/app.js
+++ Day-2/app.js
@@ -1,3 +1,9 @@
-// Skipped Day-2 for some Personal Reasons.
-// Day - 3 Begins
-let a = 10;
+// Math Functions
+
+// Math is a built-in object in JavaScript that provides properties and methods for mathematical constants and functions.
+
+// Math.round()
+// The Math.round() function returns the value of a number rounded to the nearest integer.
+
+console.log(Math.round(4.7)); // Output: 5
+console.log(Math.round(4.4)); // Output: 4
