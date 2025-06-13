821572d3b99e84246966ffa0cb01bd832ab993cf
not-developer-exe <official.additya.singh@gmail.com>
2025-06-13 14:29:27 +0530

    Area of rectangle added

---
+++ Day-3/app.js
@@ -0,0 +1,12 @@
+// Time for some basic questions:
+
+// Ques:1 Calculate the area and rectangle of a rectangle.
+
+// Solution:
+// Area:
+
+let length = 10;
+let breadth = 30;
+let area = length * breadth;
+
+console.log(`Area of rectangle is ${area}`);
