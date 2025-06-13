5604a84303649578b23f44df09bea8956ec126c0
not-developer-exe <official.additya.singh@gmail.com>
2025-06-13 14:51:55 +0530

    Area of circle added

--- Day-3/app.js
+++ Day-3/app.js
@@ -16,12 +16,30 @@
 // let len = 100;
 // let bre = 150;
 
-// let peri = len + bre;
+// let peri = 2 * (len + bre);
 
 // console.log(`Perimeter of rectangle is ${peri}`);
 
-// Generate OTP
+// Ques 2: Generate OTP
 
 // let otp = Math.floor(Math.random() * 10000) + 1000;
 
 // console.log(`OTP is ${otp}`);
+
+// Ques 3: Area of Triangle by Heron's Formula
+
+// let a = 10, b = 15, c = 17;
+
+// let s = (a + b + c) / 2;
+
+// let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+// console.log(`Area of triangle is ${area}`);
+
+// Ques 4: Area of circle
+
+// let radius = 10
+
+// let area = Math.PI * radius * radius;
+
+// console.log(`Area of circle is ${area}`);
